f5d6f18dac2bae346345d55441a01b83304cc90b
truckdrivercz <eda.orvos@seznam.cz>
2024-03-02 13:08:41 +0100

    first commit

---
+++ web/js/dark_mode.js
@@ -0,0 +1,14 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        
+        
+    }else{
+        body.style.background = 'black';
+        
+    }
+});
