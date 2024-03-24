a72dbdc9c801d5701590d36e448495d9e0f0cd24
truckdrivercz <eda.orvos@seznam.cz>
2024-03-24 10:01:18 +0100

    web

--- web/js/nabor.js
+++ web/js/nabor.js
@@ -1,7 +1,7 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
-const informations = document.getElementById("informations");
-const achievements = document.getElementById("achievements");
+const offers = document.getElementById("offers");
+const requirements = document.getElementById("requirements");
 const footer = document.getElementById("footer");
 const footerTitle = document.getElementById("footerTitle");
 const odkaz = document.getElementById("odkaz");
@@ -10,19 +10,19 @@ toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
-        informations.style.backgroundColor = "#11101b";
-        informations.style.color = "white";
-        achievements.style.backgroundColor = "#11101b";
-        achievements.style.color = "white";
+        offers.style.backgroundColor = "#11101b";
+        offers.style.color = "white";
+        requirements.style.backgroundColor = "#11101b";
+        requirements.style.color = "white";
         footer.style.backgroundColor = "#11101b";
         footerTitle.style.color = "white";
         odkaz.style.color = "red";
     } else{
         body.style.background = '#11101b';
-        informations.style.backgroundColor = "white";
-        informations.style.color = "#11101b";
-        achievements.style.backgroundColor = "white";
-        achievements.style.color = "#11101b";
+        offers.style.backgroundColor = "white";
+        offers.style.color = "#11101b";
+        requirements.style.backgroundColor = "white";
+        requirements.style.color = "#11101b";
         footer.style.backgroundColor = "white";
         footerTitle.style.color = "#11101b";
         odkaz.style.color = "red";
